10d00452d4757fc4a446377779f3843afeffdd69
PLAYMER534 <onkplaymermakuoa@gmail.com>
2025-05-30 16:10:09 +0200

    Initial commit

---
+++ SQLiteDataAdapter.cs
@@ -0,0 +1,23 @@
+﻿// SurveyApp: A polished Windows Forms survey application with modern UI elements
+
+using System.Data;
+
+namespace SurveyApp
+{
+    internal class SQLiteDataAdapter
+    {
+        private string query;
+        private SQLiteConnection conn;
+
+        public SQLiteDataAdapter(string query, SQLiteConnection conn)
+        {
+            this.query = query;
+            this.conn = conn;
+        }
+
+        internal void Fill(DataTable dt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
